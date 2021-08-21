35c14eca091d3150950aa0212fa4ea41e9692077
CipherDoc34 <63924539+CipherDoc34@users.noreply.github.com>
2021-08-21 18:48:32 -0400

    test

--- public/scripts/json.js
+++ public/scripts/json.js
@@ -1,7 +1,16 @@
 // const fs = require('fs');
 const {python} = require('child_process')
 const config = require('./test.json');
-const scrape = python('python'[/scripts/scraper.py])
+const scrape = python('python'['/scripts/scraper.py']);
+var results
+scrape.stdout.on('data', (data)=>{
+    if (data){
+        results.push();
+    }
+    else{
+        continue;
+    }
+});
 console.log(config['items'].length);
 
 // fs.readFile('./test.json', 'utf8', (err, jsonString) => {
